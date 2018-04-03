cf65dbee7b1188a0cc81145796661c31887b133a
Gia1987 <giacomo.n27@gmail.com>
2018-04-03 18:01:50 +0100

    Assert module created to implement assertion test

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,10 +1,7 @@
 (function(exports){
   function testNoteText() {
     var note = new Note('JavaScript');
-    if(note.getText() !== 'JavaScript'){
-      throw new Error('text not defined');
-  }
-  console.log('passed')
+    assert.isTrue(note.getText() === 'JavaScript')
 };
-testNoteText();
+exports.testNoteText = testNoteText();
 })(this);
